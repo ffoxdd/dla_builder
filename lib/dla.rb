48929aef0e2a6a3a98ef2bbfe909b1d904fcc0b4
Dan Fox <dhf2001@gmail.com>
2012-12-27 22:25:15 -0500

    Adds first iteration for Dla#grow

--- lib/dla.rb
+++ lib/dla.rb
@@ -2,17 +2,19 @@ require "#{File.dirname(__FILE__)}/particle.rb"
 
 class Dla
 
-  def initialize(renderer, *seeds)
+  def initialize(renderer, particle_source, *seeds)
     raise ArgumentError if seeds.empty?
 
     @renderer = renderer
+    @particle_source = particle_source
     @seeds = seeds
 
     render_seeds
   end
 
-  # def grow
-  # end
+  def grow
+    @particle_source.new
+  end
 
   protected
 
--- test/dla_test.rb
+++ test/dla_test.rb
@@ -8,23 +8,37 @@ require "#{File.dirname(__FILE__)}/../lib/dla.rb"
 describe Dla do
 
   let(:renderer) { MiniTest::Mock.new }
-  after { renderer.verify }
+  let(:particle_source) { MiniTest::Mock.new }
 
   describe "#initialize" do
     let(:seed) { MiniTest::Mock.new }
 
     it "renders the seeds" do
-      renderer.expect(:render, true, [@seed])
-      dla = Dla.new(renderer, @seed)
+      renderer.expect(:render, true, [seed])
+      dla = Dla.new(renderer, particle_source, seed)
+
+      renderer.verify
     end
 
     it "requires a seed to be passed in" do
-      -> { Dla.new(renderer) }.must_raise ArgumentError
+      -> { Dla.new(renderer, particle_source) }.must_raise ArgumentError
     end
   end
 
   describe "#grow" do
+    let(:seed) { MiniTest::Mock.new }
+    before { renderer.expect(:render, true, [seed]) }
+
+    let(:new_particle) { MiniTest::Mock.new }
+    after { particle_source.verify }
+
+    let(:dla) { Dla.new(renderer, particle_source, seed) }
+
     it "renders a new particle onto the aggregate" do
+      particle_source.expect(:new, new_particle)
+      dla.grow
+
+      particle_source.verify
     end
   end
 
