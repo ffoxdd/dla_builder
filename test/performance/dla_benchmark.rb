7aaa4cc87e887ecde001dd4f938fc73904940b5d
Dan Fox <dhf2001@gmail.com>
2013-03-29 23:58:36 -0400

    Cleans up commented out code

--- test/performance/dla_benchmark.rb
+++ test/performance/dla_benchmark.rb
@@ -10,10 +10,7 @@ require_relative '../../lib/particle.rb'
 class DlaBenchmark < MiniTest::Unit::TestCase
 
   def self.bench_range
-  	# [1, 10, 100]
   	[1, 10, 100, 1000]
-  	# [1, 5, 10, 50, 100, 250, 500, 1000]
-  	# [1, 10, 100, 250]
   end
 
   def bench_dla_growth
