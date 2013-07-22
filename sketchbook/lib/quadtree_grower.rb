ad57ec754cb0808a25b5e3ebe1d378838e562e30
Dan Fox <dan@derbyjackpot.com>
2013-07-22 15:09:37 -0400

    Irons the kinks out of the quadtree growing procedure. It's finally more performant than linear!

--- sketchbook/lib/quadtree_grower.rb
+++ sketchbook/lib/quadtree_grower.rb
@@ -21,15 +21,19 @@ class QuadtreeGrower
   attr_accessor :test_particle, :closest_particle
   attr_writer :closest_distance
 
+  def neighborhood_radius(zoom_factor = 1)
+    @neighborhood_radius * zoom_factor
+  end
+
   def spawn
     self.test_particle = particle_source.new(0, 0, radius)
     test_particle.step(spawning_radius)
-    calculate_closest
+    find_closest_particle
   end
 
   def step
     test_particle.step(step_distance)
-    calculate_closest
+    find_closest_particle
     spawn if too_far?
   end
 
@@ -43,7 +47,7 @@ class QuadtreeGrower
   end
 
   def spawning_radius
-    extent * 2
+    extent + (radius * 6)
   end
 
   def kill_radius
@@ -54,30 +58,50 @@ class QuadtreeGrower
     particles.map(&:extent).max
   end
 
-  def calculate_closest
-    self.closest_particle = closest_neighborhood_particle
+  def find_closest_particle_within_neighborhood(zoom_factor)
+    self.closest_particle = closest_neighborhood_particle(zoom_factor)
     self.closest_distance = (test_particle.distance(closest_particle) if closest_particle)
 
-    if closest_distance > neighborhood_radius
-      self.closest_particle = nil
-      self.closest_distance = nil
-    end
+    reset_closest_particle_information if false_closest_particle?(zoom_factor)
   end
 
-  def neighborhood_range(center)
-    (center - search_radius)..(center + search_radius)
+  def false_closest_particle?(zoom_factor)
+    closest_distance > neighborhood_radius(zoom_factor)
   end
 
-  def search_radius
-    neighborhood_radius + radius
+  def find_closest_particle
+    reset_closest_particle_information
+    zoom_factor = 1
+
+    begin
+      find_closest_particle_within_neighborhood(zoom_factor)
+      zoom_factor += 1
+    end until closest_particle_found?
   end
 
-  def neighborhood_particles
-    particles.within(neighborhood_range(test_particle.x), neighborhood_range(test_particle.y))
+  def reset_closest_particle_information
+    self.closest_particle = nil
+    self.closest_distance = nil
   end
 
-  def closest_neighborhood_particle
-    neighborhood_particles.min_by { |particle| test_particle.distance(particle) }
+  def closest_particle_found?
+    !!self.closest_particle
+  end
+
+  def neighborhood_range(center, zoom_factor)
+    (center - search_radius(zoom_factor))..(center + search_radius(zoom_factor))
+  end
+
+  def search_radius(zoom_factor)
+    neighborhood_radius(zoom_factor) + radius
+  end
+
+  def neighborhood_particles(zoom_factor)
+    particles.within(neighborhood_range(test_particle.x, zoom_factor), neighborhood_range(test_particle.y, zoom_factor))
+  end
+
+  def closest_neighborhood_particle(zoom_factor)
+    neighborhood_particles(zoom_factor).min_by { |particle| test_particle.distance(particle) }
   end
 
   def closest_distance
