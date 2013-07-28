431de07d99edec175cd7b39bee4c010f162597c6
Dan Fox <dan@derbyjackpot.com>
2013-07-28 16:07:02 -0400

    Makes the Quadtree act on points, not particles

--- sketchbook/lib/quadtree.rb
+++ sketchbook/lib/quadtree.rb
@@ -8,38 +8,38 @@ class Quadtree
     @x_range = x_range
     @y_range = y_range
     @max_depth = options.fetch(:max_depth) { 6 }
-    @particles = []
+    @points = []
     @children = []
   end
 
   def size
-    leaf? ? particles.size : children.map(&:size).inject(&:+)
+    leaf? ? points.size : children.map(&:size).inject(&:+)
   end
 
-  def <<(particle)
-    return unless covers?(particle)
+  def <<(point)
+    return unless covers?(point)
     subdivide if leaf? && can_subdivide?
-    (leaf? ? particles : child_for(particle)) << particle
+    (leaf? ? points : child_for(point)) << point
   end
 
   def each(&block)
     if leaf?
-      particles.each(&block)
+      points.each(&block)
     else
       children.each { |child| child.each(&block) }
     end
   end
 
-  def covers?(particle)
-    x_range.include?(particle.x) && y_range.include?(particle.y)
+  def covers?(point)
+    x_range.include?(point.x) && y_range.include?(point.y)
   end
 
   def within(test_x_range, test_y_range)
     return [] unless intersects?(test_x_range, test_y_range)
 
     if leaf?
-      @particles.select do |particle|
-        test_x_range.include?(particle.x) && test_y_range.include?(particle.y)
+      @points.select do |point|
+        test_x_range.include?(point.x) && test_y_range.include?(point.y)
       end
     else
       children.map { |child| child.within(test_x_range, test_y_range) }.flatten # TODO: use flat_map after upgrading ruby
@@ -53,7 +53,7 @@ class Quadtree
 
   private
 
-  attr_reader :x_range, :y_range, :particles, :max_depth, :children
+  attr_reader :x_range, :y_range, :points, :max_depth, :children
 
   def intersects?(test_x_range, test_y_range)
     x_range_intersects?(test_x_range) && y_range_intersects?(test_y_range)
@@ -67,12 +67,12 @@ class Quadtree
     RangeIntersectionCalculator.new(y_range, range).intersect?
   end
 
-  def child_for(particle)
-    children[child_index_for(particle)]
+  def child_for(point)
+    children[child_index_for(point)]
   end
 
-  def child_index_for(particle)
-    (particle.x >= midpoint(x_range) ? 1 : 0) + (particle.y >= midpoint(y_range) ? 2 : 0)
+  def child_index_for(point)
+    (point.x >= midpoint(x_range) ? 1 : 0) + (point.y >= midpoint(y_range) ? 2 : 0)
   end
 
   def can_subdivide?
