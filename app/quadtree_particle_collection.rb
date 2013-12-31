58a92e82d339f9980c59a2bbed8775eb7c412508
Dan Fox <dan@derbyjackpot.com>
2013-12-31 13:38:26 -0500

    Maintains an array of added particles in QuadtreeParticleCollection in addition to the spacial quadtree

--- app/quadtree_particle_collection.rb
+++ app/quadtree_particle_collection.rb
@@ -7,11 +7,12 @@ class QuadtreeParticleCollection
   include Enumerable
   extend Forwardable
 
-  def_delegators :quadtree, :size, :<<, :each
+  def_delegators :particles, :size, :each
 
   def initialize(particle_radius)
     bounding_box = BoundingBox.new(-2000..2000, -2000..2000)
     @quadtree = Quadtree.new(bounding_box)
+    @particles = []
     @particle_radius = particle_radius
   end
 
@@ -19,9 +20,14 @@ class QuadtreeParticleCollection
     closest_particle_finder(test_particle).closest_particle
   end
 
+  def <<(particle)
+    quadtree << particle
+    particles << particle
+  end
+
   private
 
-    attr_reader :quadtree, :particle_radius
+    attr_reader :quadtree, :particles, :particle_radius
 
     def closest_particle_finder(test_particle)
       QuadtreeClosestParticleFinder.new(quadtree, test_particle, particle_radius)
