ec5f12a7bc81fce7ea408614bcfb9aa8a1140d66
Dan Fox <dan@derbyjackpot.com>
2014-01-19 20:12:47 -0500

    Improves the basic growth feature test

--- test/integration/simple_growth_feature.rb
+++ test/integration/simple_growth_feature.rb
@@ -1,13 +1,15 @@
 require_relative "../test_helper.rb"
 require_relative '../../app/dla.rb'
 
-describe "Linear DLA Growth" do
-  let(:seed) { [Particle.new(0, 0, 2)] }
-  let(:dla) { Dla.new }
+describe "Simple DLA Growth" do
 
-  it "does not blow up after growing several particles" do
+  let(:seed) { [Particle.new(radius: 1)] }
+  let(:dla) { Dla.new(radius: 1, seeds: seed) }
+
+  it "grows particles onto an aggregate" do
     -> { 10.times { dla.grow } }.must_be_silent
     dla.size.must_equal 11
+    dla.within?(BoundingBox.new(-3..3, -3..3)).must_equal false
   end
 
 end
