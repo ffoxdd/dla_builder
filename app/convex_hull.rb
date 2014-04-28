16babc93d8951c12f45bc011fef298859377031c
Dan Fox <dan@derbyjackpot.com>
2014-04-27 20:02:37 -0400

    Fully implements ConvexHull

--- app/convex_hull.rb
+++ app/convex_hull.rb
@@ -16,28 +16,80 @@ class ConvexHull
       loop do
         yield(current_node.element)
         current_node = current_node.next_node
-        break if current_node == root
+        return if current_node == root
       end
     end
   end
 
   def add_point(point)
-    insert_point(point, root, root)
+    if singleton?
+      insert_point(point, root, root)
+      return
+    end
+
+    n0 = lower_tangency_node(point)
+    n1 = upper_tangency_node(point)
+
+    return unless n0 && n1
+    insert_point(point, n0, n1)
   end
 
   private
 
     attr_reader :root
 
+    def singleton?
+      root.next_node == root
+    end
+
+    def upper_tangency_node(point)
+      current_node = root
+
+      loop do
+        return current_node if upper_tangency_point?(point, current_node)
+        current_node = current_node.next_node
+        return if current_node == root
+      end
+    end
+
+    def lower_tangency_node(point)
+      current_node = root
+
+      loop do
+        return current_node if lower_tangency_point?(point, current_node)
+        current_node = current_node.previous_node
+        return if current_node == root
+      end
+    end
+
+    def upper_tangency_point?(point, node)
+      can_see?(point, node.previous_edge) && !can_see?(point, node.next_edge)
+    end
+
+    def lower_tangency_point?(point, node)
+      !can_see?(point, node.previous_edge) && can_see?(point, node.next_edge)
+    end
+
+    def can_see?(point, edge)
+      v0 = edge[1] - edge[0]
+      v1 = point - edge[0]
+
+      determinant(v0, v1) > 0
+    end
+
+    def determinant(v0, v1)
+      v0[0] * v1[1] - v0[1] * v1[0]
+    end
+
     def self_link_root
       root.link_next(root)
       root.link_previous(root)
     end
 
-    def insert_point(point, p0, p1)
+    def insert_point(point, n0, n1)
       node = LinkedList.new(point)
-      p0.link_next(node)
-      p1.link_previous(node)
+      n0.link_next(node)
+      n1.link_previous(node)
     end
 
 end
--- test/unit/convex_hull_test.rb
+++ test/unit/convex_hull_test.rb
@@ -20,7 +20,27 @@ describe ConvexHull do
       convex_hull = ConvexHull.new(p0)
 
       convex_hull.add_point(p1)
-      convex_hull.points.must_equal [p0, p1]
+      convex_hull.points.must_equal [p0, p1] # simple addition
+
+      p2 = Point.new(1, 1)
+      convex_hull.add_point(p2)
+      convex_hull.points.must_equal [p0, p2, p1] # maintains clockwise order
+
+      p3 = Point.new(0.5, 0.25)
+      convex_hull.add_point(p3)
+      convex_hull.points.must_equal [p0, p2, p1] # ignores interior points
+
+      p4 = Point.new(0, 1)
+      convex_hull.add_point(p4)
+      convex_hull.points.must_equal [p0, p4, p2, p1]
+
+      p5 = Point.new(2, 2)
+      convex_hull.add_point(p5)
+      convex_hull.points.must_equal [p0, p4, p5, p1] # removes superceded points
+
+      p6 = Point.new(0.5, 0)
+      convex_hull.add_point(p6)
+      convex_hull.points.must_equal [p0, p4, p5, p1] # ignores boundary points
     end
   end
 
