3997eb3a65f79f09d6f119445678367671c314cc
Dan Fox <dan@derbyjackpot.com>
2015-11-29 15:29:40 -0500

    Comments out test related to the old scheme

--- app/dcel/half_edge.rb
+++ app/dcel/half_edge.rb
@@ -19,69 +19,69 @@ class DCEL::HalfEdge
 
   attr_reader :origin, :previous_half_edge, :next_half_edge, :twin_half_edge
 
-  def self.singleton(options = {})
-    new(options).tap(&:self_link)
-  end
-
-  def singleton?
-    twin_half_edge == self
-  end
-
-  def self_link
-    link_all(self)
-  end
-
-  def link_vertex(vertex)
-    (link_degenerate_vertex(vertex) && return) if singleton?
-
-    new_half_edge = DCEL::HalfEdge.new(origin: vertex)
-
-    new_half_edge.link_next(next_half_edge)
-    new_half_edge.link_previous(self)
-
-    new_twin = DCEL::HalfEdge.new(origin: new_half_edge.next_half_edge.origin)
-    new_next_twin = DCEL::HalfEdge.new(origin: previous_half_edge.origin)
-    new_previous_twin = DCEL::HalfEdge.new(origin: new_half_edge.origin)
-
-    new_twin.link_previous(new_next_twin)
-    new_twin.link_next(new_previous_twin)
-    new_next_twin.link_previous(new_previous_twin)
-
-    link_twin(new_twin)
-    previous_half_edge.link_twin(new_previous_twin)
-    next_half_edge.link_twin(new_next_twin)
-  end
-
-  def link_degenerate_vertex(vertex)
-    new_half_edge = DCEL::HalfEdge.new(
-      origin: vertex,
-      previous_half_edge: self,
-      next_half_edge: self,
-      twin_half_edge: self
-    )
-
-    link_all(new_half_edge)
-  end
+  # def self.singleton(options = {})
+  #   new(options).tap(&:self_link)
+  # end
+  #
+  # def singleton?
+  #   twin_half_edge == self
+  # end
+  #
+  # def self_link
+  #   link_all(self)
+  # end
+  #
+  # def link_vertex(vertex)
+  #   (link_degenerate_vertex(vertex) && return) if singleton?
+  #
+  #   new_half_edge = DCEL::HalfEdge.new(origin: vertex)
+  #
+  #   new_half_edge.link_next(next_half_edge)
+  #   new_half_edge.link_previous(self)
+  #
+  #   new_twin = DCEL::HalfEdge.new(origin: new_half_edge.next_half_edge.origin)
+  #   new_next_twin = DCEL::HalfEdge.new(origin: previous_half_edge.origin)
+  #   new_previous_twin = DCEL::HalfEdge.new(origin: new_half_edge.origin)
+  #
+  #   new_twin.link_previous(new_next_twin)
+  #   new_twin.link_next(new_previous_twin)
+  #   new_next_twin.link_previous(new_previous_twin)
+  #
+  #   link_twin(new_twin)
+  #   previous_half_edge.link_twin(new_previous_twin)
+  #   next_half_edge.link_twin(new_next_twin)
+  # end
+  #
+  # def link_degenerate_vertex(vertex)
+  #   new_half_edge = DCEL::HalfEdge.new(
+  #     origin: vertex,
+  #     previous_half_edge: self,
+  #     next_half_edge: self,
+  #     twin_half_edge: self
+  #   )
+  #
+  #   link_all(new_half_edge)
+  # end
 
   protected
 
   attr_writer :origin, :next_half_edge, :previous_half_edge, :twin_half_edge
   attr_reader :id # utility / for testing
 
-  def link_next(half_edge)
-    self.next_half_edge = half_edge
-    half_edge.previous_half_edge = self
-  end
-
-  def link_previous(half_edge)
-    self.previous_half_edge = half_edge
-    half_edge.next_half_edge = self
-  end
-
-  def link_twin(half_edge)
-    self.twin_half_edge = half_edge
-    half_edge.twin_half_edge = self
-  end
+  # def link_next(half_edge)
+  #   self.next_half_edge = half_edge
+  #   half_edge.previous_half_edge = self
+  # end
+  #
+  # def link_previous(half_edge)
+  #   self.previous_half_edge = half_edge
+  #   half_edge.next_half_edge = self
+  # end
+  #
+  # def link_twin(half_edge)
+  #   self.twin_half_edge = half_edge
+  #   half_edge.twin_half_edge = self
+  # end
 
   private
 
@@ -91,10 +91,10 @@ class DCEL::HalfEdge
     end.join(", ")
   end
 
-  def link_all(half_edge)
-    self.previous_half_edge = half_edge
-    self.next_half_edge = half_edge
-    self.twin_half_edge = half_edge
-  end
+  # def link_all(half_edge)
+  #   self.previous_half_edge = half_edge
+  #   self.next_half_edge = half_edge
+  #   self.twin_half_edge = half_edge
+  # end
 
 end
--- test/unit/dcel/half_edge_test.rb
+++ test/unit/dcel/half_edge_test.rb
@@ -39,80 +39,80 @@ describe DCEL::HalfEdge do
     end
   end
 
-  describe ".singleton" do
-    it "creates a half-edge that is it's own twin, next and previous edge" do
-      origin = test_vertex
-
-      half_edge = DCEL::HalfEdge.singleton(origin: origin)
-      half_edge.twin_half_edge.must_equal(half_edge)
-      half_edge.next_half_edge.must_equal(half_edge)
-      half_edge.previous_half_edge.must_equal(half_edge)
-    end
-  end
-
-  describe "#singleton?" do
-    it "returns true for a singleton (self-linked half edge)" do
-      vertex_0 = test_vertex
-      vertex_1 = test_vertex
-
-      half_edge = DCEL::HalfEdge.singleton(origin: vertex_0)
-      half_edge.singleton?.must_equal(true)
-
-      half_edge.link_vertex(vertex_1)
-      half_edge.singleton?.must_equal(false)
-    end
-  end
-
-  describe "#link_vertex" do
-    it "links a singleton half edge to a new vertex by creating a new half edge" do
-      vertex_0 = Object.new
-      vertex_1 = Object.new
-
-      half_edge_0 = DCEL::HalfEdge.singleton(origin: vertex_0)
-
-      half_edge_0.link_vertex(vertex_1)
-      half_edge_1 = half_edge_0.twin_half_edge
-
-      half_edge_1.wont_equal(half_edge_0)
-      half_edge_1.origin.must_equal(vertex_1)
-
-      half_edge_0.previous_half_edge.must_equal(half_edge_1)
-      half_edge_0.next_half_edge.must_equal(half_edge_1)
-      half_edge_0.twin_half_edge.must_equal(half_edge_1)
-
-      half_edge_1.previous_half_edge.must_equal(half_edge_0)
-      half_edge_1.next_half_edge.must_equal(half_edge_0)
-      half_edge_1.twin_half_edge.must_equal(half_edge_0)
-    end
-
-    it "links a connected half edge by connecting a new triangle" do
-      vertex_0 = Object.new
-      vertex_1 = Object.new
-      vertex_2 = Object.new
-
-      half_edge_0 = DCEL::HalfEdge.singleton(origin: vertex_0)
-
-      half_edge_0.link_vertex(vertex_1)
-      half_edge_1 = half_edge_0.next_half_edge
-
-      half_edge_1.link_vertex(vertex_2)
-      half_edge_2 = half_edge_1.next_half_edge
-
-      half_edge_2.wont_equal(half_edge_0) # temp
-      half_edge_2.wont_equal(half_edge_1) # temp
-
-      half_edge_2.previous_half_edge.must_equal(half_edge_1)
-      half_edge_2.next_half_edge.must_equal(half_edge_0)
-
-      half_edge_0.previous_half_edge.must_equal(half_edge_2)
-      half_edge_1.next_half_edge.must_equal(half_edge_2)
-
-      new_twin = half_edge_2.twin_half_edge
-      new_twin.origin.must_equal(vertex_0)
-
-      new_twin.previous_half_edge.must_equal(half_edge_0.twin_half_edge)
-      new_twin.next_half_edge.must_equal(half_edge_1.twin_half_edge)
-    end
-  end
+  # describe ".singleton" do
+  #   it "creates a half-edge that is it's own twin, next and previous edge" do
+  #     origin = test_vertex
+  #
+  #     half_edge = DCEL::HalfEdge.singleton(origin: origin)
+  #     half_edge.twin_half_edge.must_equal(half_edge)
+  #     half_edge.next_half_edge.must_equal(half_edge)
+  #     half_edge.previous_half_edge.must_equal(half_edge)
+  #   end
+  # end
+  #
+  # describe "#singleton?" do
+  #   it "returns true for a singleton (self-linked half edge)" do
+  #     vertex_0 = test_vertex
+  #     vertex_1 = test_vertex
+  #
+  #     half_edge = DCEL::HalfEdge.singleton(origin: vertex_0)
+  #     half_edge.singleton?.must_equal(true)
+  #
+  #     half_edge.link_vertex(vertex_1)
+  #     half_edge.singleton?.must_equal(false)
+  #   end
+  # end
+  #
+  # describe "#link_vertex" do
+  #   it "links a singleton half edge to a new vertex by creating a new half edge" do
+  #     vertex_0 = Object.new
+  #     vertex_1 = Object.new
+  #
+  #     half_edge_0 = DCEL::HalfEdge.singleton(origin: vertex_0)
+  #
+  #     half_edge_0.link_vertex(vertex_1)
+  #     half_edge_1 = half_edge_0.twin_half_edge
+  #
+  #     half_edge_1.wont_equal(half_edge_0)
+  #     half_edge_1.origin.must_equal(vertex_1)
+  #
+  #     half_edge_0.previous_half_edge.must_equal(half_edge_1)
+  #     half_edge_0.next_half_edge.must_equal(half_edge_1)
+  #     half_edge_0.twin_half_edge.must_equal(half_edge_1)
+  #
+  #     half_edge_1.previous_half_edge.must_equal(half_edge_0)
+  #     half_edge_1.next_half_edge.must_equal(half_edge_0)
+  #     half_edge_1.twin_half_edge.must_equal(half_edge_0)
+  #   end
+  #
+  #   it "links a connected half edge by connecting a new triangle" do
+  #     vertex_0 = Object.new
+  #     vertex_1 = Object.new
+  #     vertex_2 = Object.new
+  #
+  #     half_edge_0 = DCEL::HalfEdge.singleton(origin: vertex_0)
+  #
+  #     half_edge_0.link_vertex(vertex_1)
+  #     half_edge_1 = half_edge_0.next_half_edge
+  #
+  #     half_edge_1.link_vertex(vertex_2)
+  #     half_edge_2 = half_edge_1.next_half_edge
+  #
+  #     half_edge_2.wont_equal(half_edge_0) # temp
+  #     half_edge_2.wont_equal(half_edge_1) # temp
+  #
+  #     half_edge_2.previous_half_edge.must_equal(half_edge_1)
+  #     half_edge_2.next_half_edge.must_equal(half_edge_0)
+  #
+  #     half_edge_0.previous_half_edge.must_equal(half_edge_2)
+  #     half_edge_1.next_half_edge.must_equal(half_edge_2)
+  #
+  #     new_twin = half_edge_2.twin_half_edge
+  #     new_twin.origin.must_equal(vertex_0)
+  #
+  #     new_twin.previous_half_edge.must_equal(half_edge_0.twin_half_edge)
+  #     new_twin.next_half_edge.must_equal(half_edge_1.twin_half_edge)
+  #   end
+  # end
 
 end
