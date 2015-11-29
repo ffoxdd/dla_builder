1e5ac25c03e42f5c9f5ca2fa4e518cebedc60f2b
Dan Fox <dan@derbyjackpot.com>
2015-11-28 19:08:53 -0500

    Adds a more useful inspect method

--- app/dcel/half_edge.rb
+++ app/dcel/half_edge.rb
@@ -2,11 +2,19 @@ module DCEL; end
 
 class DCEL::HalfEdge
 
+  @@instance_count = 0
+
   def initialize(options = {})
     @origin = options.fetch(:origin)
     @previous_half_edge = options.fetch(:previous_half_edge, nil)
     @next_half_edge = options.fetch(:next_half_edge, nil)
     @twin_half_edge = options.fetch(:twin_half_edge, nil)
+
+    @id = (@@instance_count += 1)
+  end
+
+  def inspect
+    "#<#{self.class.name}:#{id} origin=#{origin.object_id} #{inspect_links}>"
   end
 
   attr_reader :origin, :previous_half_edge, :next_half_edge, :twin_half_edge
@@ -28,5 +36,14 @@ class DCEL::HalfEdge
   protected
 
   attr_writer :origin, :next_half_edge, :previous_half_edge, :twin_half_edge
+  attr_reader :id # utility / for testing
+
+  private
+
+  def inspect_links
+    [:previous_half_edge, :next_half_edge, :twin_half_edge].map do |m|
+      "#{m}=#{send(m).id if send(m)}"
+    end.join(", ")
+  end
 
 end
