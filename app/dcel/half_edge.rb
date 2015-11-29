a500772f0f45a486e91d15e440c29b02afbac050
Dan Fox <dan@derbyjackpot.com>
2015-11-29 16:07:17 -0500

    Removes some commented out code

--- app/dcel/half_edge.rb
+++ app/dcel/half_edge.rb
@@ -48,59 +48,15 @@ class DCEL::HalfEdge
     enumerable.cycle.each_cons(2).take(enumerable.size).each(&block)
   end
 
-  # def self.singleton(options = {})
-  #   new(options).tap(&:self_link)
-  # end
-  #
-  # def singleton?
-  #   twin_half_edge == self
-  # end
-  #
-  # def self_link
-  #   link_all(self)
-  # end
-  #
-  # def link_vertex(vertex)
-  #   (link_degenerate_vertex(vertex) && return) if singleton?
-  #
-  #   new_half_edge = DCEL::HalfEdge.new(origin: vertex)
-  #
-  #   new_half_edge.link_next(next_half_edge)
-  #   new_half_edge.link_previous(self)
-  #
-  #   new_twin = DCEL::HalfEdge.new(origin: new_half_edge.next_half_edge.origin)
-  #   new_next_twin = DCEL::HalfEdge.new(origin: previous_half_edge.origin)
-  #   new_previous_twin = DCEL::HalfEdge.new(origin: new_half_edge.origin)
-  #
-  #   new_twin.link_previous(new_next_twin)
-  #   new_twin.link_next(new_previous_twin)
-  #   new_next_twin.link_previous(new_previous_twin)
-  #
-  #   link_twin(new_twin)
-  #   previous_half_edge.link_twin(new_previous_twin)
-  #   next_half_edge.link_twin(new_next_twin)
-  # end
-  #
-  # def link_degenerate_vertex(vertex)
-  #   new_half_edge = DCEL::HalfEdge.new(
-  #     origin: vertex,
-  #     previous_half_edge: self,
-  #     next_half_edge: self,
-  #     twin_half_edge: self
-  #   )
-  #
-  #   link_all(new_half_edge)
-  # end
-
   protected
 
   # attr_writer :origin
   attr_reader :id # utility / for testing
 
-  def link_next(half_edge)
-    self.next_half_edge = half_edge
-    half_edge.previous_half_edge = self
-  end
+  # def link_next(half_edge)
+  #   self.next_half_edge = half_edge
+  #   half_edge.previous_half_edge = self
+  # end
 
   # def link_previous(half_edge)
   #   self.previous_half_edge = half_edge
@@ -119,11 +75,5 @@ class DCEL::HalfEdge
       "#{m}=#{send(m).id if send(m)}"
     end.join(", ")
   end
-
-  # def link_all(half_edge)
-  #   self.previous_half_edge = half_edge
-  #   self.next_half_edge = half_edge
-  #   self.twin_half_edge = half_edge
-  # end
 
 end
