3deb8b807916a6f3f4c47f4dd878ec7256f48d6b
Dan Fox <dan@derbyjackpot.com>
2015-12-07 12:38:05 -0500

    Updates the builder test to use the new models. Removes old test helper code

--- test/support/dcel_test_helper.rb
+++ test/support/dcel_test_helper.rb
@@ -8,24 +8,3 @@ end
 def test_edge
   DCEL::Edge.new(origin_vertex: test_vertex)
 end
-
-module MiniTest::Assertions
-  def assert_edge_cycle(_, edges) # TODO: find a better place for this assertion
-    assert cycle?(edges), "Expected edges to form a cycle"
-  end
-
-  private
-
-  def cycle?(edges)
-    DCEL.cyclical_each_pair(edges).all? do |previous_edge, next_edge|
-      sequentially_linked?(previous_edge, next_edge)
-    end
-  end
-
-  def sequentially_linked?(previous_edge, next_edge)
-    previous_edge.next_edge == next_edge &&
-    next_edge.previous_edge == previous_edge
-  end
-end
-
-Array.infect_an_assertion :assert_edge_cycle, :must_form_a_edge_cycle
--- test/unit/dcel/builder_test.rb
+++ test/unit/dcel/builder_test.rb
@@ -3,24 +3,19 @@ require_relative "../../support/dcel_test_helper.rb"
 require_relative "../../../app/dcel/builder"
 
 describe DCEL::Builder do
+
   describe ".face" do
-    let(:vertices) { 3.times.map { test_vertex }}
+    let(:vertices) { 3.times.to_a }
 
     it "creates a fully linked face" do
-      face = DCEL::Builder.face(vertices)
-      face.vertices.must_equal(vertices)
+      inner_face = DCEL::Builder.face(vertices)
+      outer_face = inner_face.opposite_face
 
-      inner_edges = face.edges
-      inner_edges.must_form_a_edge_cycle
+      inner_face.vertices.must_cyclically_equal(vertices)
+      outer_face.vertices.must_cyclically_equal(vertices.reverse)
 
-      outer_edges = inner_edges.map(&:opposite_edge)
-      outer_edges.map(&:origin_vertex).must_equal([vertices[1], vertices[2], vertices[0]])
-      outer_edges.reverse.must_form_a_edge_cycle
-
-      inner_edges.each { |edge| edge.left_face.must_equal(face) }
-
-      outer_face = outer_edges.first.left_face
-      outer_edges.each { |edge| edge.left_face.must_equal(outer_face) }
+      inner_face.each_edge { |edge| edge.left_face.must_equal(inner_face) }
+      outer_face.each_edge { |edge| edge.left_face.must_equal(outer_face) }
     end
   end
 
