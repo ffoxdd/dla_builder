2043ad8b04fea808580f9688f722d6c10a171bbc
Dan Fox <dan@derbyjackpot.com>
2015-12-07 01:33:44 -0500

    Make the vertex deleter test use the new signature

--- test/unit/dcel/vertex_deleter_test.rb
+++ test/unit/dcel/vertex_deleter_test.rb
@@ -22,12 +22,15 @@ describe DCEL::VertexDeleter do
 
     it "can delete an inner vertex" do
       inner_vertex_edge = original_edges[0].previous_edge
-      DCEL::VertexDeleter.delete_vertex(inner_vertex_edge)
 
-      inner_edge = original_edges[0]
+      DCEL::VertexDeleter.delete_vertex(inner_vertex_edge) do |deleted_faces, deleted_edges, deleted_vertex|
+        inner_edge = original_edges[0]
 
-      inner_edge.must_be_face_for(vertices)
-      inner_edge.opposite_edge.must_be_face_for([vertices[1], vertices[0], vertices[2]])
+        inner_edge.must_be_face_for(vertices)
+        inner_edge.opposite_edge.must_be_face_for([vertices[1], vertices[0], vertices[2]])
+
+        remaining_edges = inner_edge.each_next_edge.to_a
+      end
     end
 
     it "can delete a perimeter vertex" do
