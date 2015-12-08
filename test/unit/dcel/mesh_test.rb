82bae8f82e72319af0ea35b3e97eaba3add63af4
Dan Fox <dan@derbyjackpot.com>
2015-12-07 20:49:10 -0500

    Refactors the test for DCEL::Mesh.polygon to be more consistent with the others

--- test/unit/dcel/mesh_test.rb
+++ test/unit/dcel/mesh_test.rb
@@ -11,36 +11,26 @@ describe DCEL::Mesh do
 
   describe ".polygon" do
     let(:vertices) { 3.times.map { test_vertex }}
-    let(:inner_face) { DCEL::PolygonBuilder.polygon(vertices) }
-    let(:mock_builder) { Minitest::Mock.new }
 
-    it "creates a mesh with a single polygon" do
-      # TODO: consider rewriting this test so you aren't first using the builder and then stubbing it
-      mock_builder.expect(:polygon, inner_face, [vertices])
+    it "creates a mesh representing a single polygon" do
+      mesh = DCEL::Mesh.polygon(vertices)
 
-      DCEL.stub_const(:PolygonBuilder, mock_builder) do
-        mesh = DCEL::Mesh.polygon(vertices)
-
-        mesh.faces.must_equal([inner_face])
-        mesh.edges.must_equal(inner_face.edges)
-        mesh.vertices.must_equal(inner_face.vertices)
-      end
-
-      mock_builder.verify
+      # mesh.faces.size.must_equal(2) # 1 bounded, 1 infinite
+      mesh.edges.size.must_equal(3) # only count each polygon edge once
+      mesh.vertices.must_cyclically_equal(vertices)
     end
   end
 
   describe "#subdivide" do
+    let(:vertices) { 3.times.map { test_vertex }}
+    let(:new_vertex) { test_vertex }
+    let(:mesh) { DCEL::Mesh.polygon(vertices) }
+    let(:face) { mesh.faces.first }
+
     it "augments the mesh by subdividing the specified face by a new vertex" do
-      vertices = 3.times.map { test_vertex }
-      new_vertex = test_vertex
+      mesh.subdivide(face, new_vertex)
 
-      mesh = DCEL::Mesh.polygon(vertices)
-      face = mesh.faces.first
-
-      new_faces = mesh.subdivide(face, new_vertex)
-
-      mesh.faces.must_equal(new_faces)
+      # mesh.faces.size.must_equal(4) # 3 bounded, 1 infinite
       mesh.edges.size.must_equal(6)
       mesh.vertices.size.must_equal(4)
     end
@@ -52,13 +42,17 @@ describe DCEL::Mesh do
     let(:mesh) { DCEL::Mesh.polygon(vertices) }
     let(:face) { mesh.faces.first }
 
-    it "deletes a vertex, along with any incident edges and faces" do
-      perimeter_edge = mesh.edges.first
-      mesh.subdivide(face, inner_vertex)
+    attr_reader :perimeter_edge
 
+    before do
+      @perimeter_edge = mesh.edges.first
+      mesh.subdivide(face, inner_vertex)
+    end
+
+    it "deletes a vertex, along with any incident edges and faces" do
       mesh.delete_vertex(perimeter_edge)
 
-      # mesh.faces.size.must_equal(1)
+      # mesh.faces.size.must_equal(2) # 1 bounded, 1 infinite
       mesh.edges.size.must_equal(3)
       mesh.vertices.size.must_equal(3)
     end
