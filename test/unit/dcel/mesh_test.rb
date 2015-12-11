82d4ef3ee4bac080ee206d2c6890b6e910432936
Dan Fox <dan@derbyjackpot.com>
2015-12-11 13:29:15 -0500

    Fills out the test for Mesh a bit

--- test/unit/dcel/mesh_test.rb
+++ test/unit/dcel/mesh_test.rb
@@ -18,6 +18,8 @@ describe DCEL::Mesh do
 
       mesh.faces.size.must_equal(2) # 1 bounded, 1 infinite
       mesh.edges.size.must_equal(3) # only count each polygon edge once
+      mesh.vertices.size.must_equal(3)
+
       mesh.vertex_values.must_cyclically_equal(vertex_values)
     end
   end
@@ -34,21 +36,25 @@ describe DCEL::Mesh do
       mesh.faces.size.must_equal(4) # 3 bounded, 1 infinite
       mesh.edges.size.must_equal(6)
       mesh.vertices.size.must_equal(4)
-      Set.new(mesh.vertex_values).must_equal(Set.new(vertex_values + [new_vertex_value]))
+
+      new_vertex_values = vertex_values + [new_vertex_value]
+      Set.new(mesh.vertex_values).must_equal(Set.new(new_vertex_values))
     end
   end
 
   describe "#delete_vertex" do
-    let(:vertices) { 3.times.map { test_vertex } }
-    let(:inner_vertex) { test_vertex }
-    let(:mesh) { DCEL::Mesh.polygon(vertices) }
+    let(:perimeter_vertex_values) { 3.times.map { test_vertex } }
+    let(:inner_vertex_value) { test_vertex }
+    let(:vertex_values) { perimeter_vertex_values + [inner_vertex_value] }
+    let(:mesh) { DCEL::Mesh.polygon(perimeter_vertex_values) }
     let(:face) { mesh.faces.first }
 
-    attr_reader :perimeter_edge
+    attr_reader :perimeter_edge, :deleted_vertex
 
     before do
       @perimeter_edge = mesh.edges.first
-      mesh.subdivide(face, inner_vertex)
+      @deleted_vertex = @perimeter_edge.origin_vertex
+      mesh.subdivide(face, inner_vertex_value)
     end
 
     it "deletes a vertex, along with any incident edges and faces" do
@@ -57,6 +63,9 @@ describe DCEL::Mesh do
       mesh.faces.size.must_equal(2) # 1 bounded, 1 infinite
       mesh.edges.size.must_equal(3)
       mesh.vertices.size.must_equal(3)
+
+      remaining_vertex_values = vertex_values - [deleted_vertex.value]
+      Set.new(mesh.vertex_values).must_equal(Set.new(remaining_vertex_values))
     end
   end
 
